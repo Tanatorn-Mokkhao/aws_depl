6815fde061a72073864f5dfcdf4712ab8db154d5
Tanatorn Mokkhao <tanatorn.mokkhao@gmail.com>
2022-08-17 11:46:18 +0700

    yml add

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express'
 const app = express()
 
+app.use('/', (req,res) => {
+  res.send('Hello aws world')
+})
 
 app.listen(3000, () => {
   console.log('server is running on port 3000')
